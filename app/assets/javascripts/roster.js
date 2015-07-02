22b1991e7c4882d0b06af40413ca3b2130df2dfb
James Dabbs <jamesdabbs@gmail.com>
2015-07-02 11:23:24 -0400

    Workingish

--- app/assets/javascripts/roster.js
+++ app/assets/javascripts/roster.js
@@ -14,4 +14,20 @@ $(document).on("ready", function() {
       }
     })
   })
+
+  $(".player-name").on("click", function() {
+    var current_name = $(this).text()
+    var input = $("<input type='text'>").val(current_name)
+
+    $(this).replaceWith(input)
+    input.focus()
+
+    input.on("blur", function() {
+      var updated_name = $(this).val()
+      $(this).replaceWith(updated_name)
+      alert(updated_name)
+    })
+
+    console.log("name", current_name)
+  })
 })
