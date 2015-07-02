12c7b67af06d5d17ca2e9aa80a1ad8a92f9a2973
James Dabbs <jamesdabbs@gmail.com>
2015-07-02 10:33:56 -0400

    Pass sort order back to the server

--- app/assets/javascripts/roster.js
+++ app/assets/javascripts/roster.js
@@ -1,3 +1,14 @@
 $(document).on("ready", function() {
   $("#roster tbody").sortable()
+
+  $("button").on("click", function() {
+    var sort_order = $("#roster tbody").sortable("serialize", { key: "player_id" })
+
+    $.ajax(window.location + "/reorder", {
+      method: "POST",
+      data: {
+        order: sort_order
+      }
+    })
+  })
 })
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,8 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find params[:id]
   end
+
+  def reorder
+    binding.pry
+  end
 end
