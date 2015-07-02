8583cae2253bedb65a53957ee199416a679deb3f
James Dabbs <jamesdabbs@gmail.com>
2015-07-02 10:38:08 -0400

    Better array param passing

--- app/assets/javascripts/roster.js
+++ app/assets/javascripts/roster.js
@@ -2,13 +2,13 @@ $(document).on("ready", function() {
   $("#roster tbody").sortable()
 
   $("button").on("click", function() {
-    var sort_order = $("#roster tbody").sortable("serialize", { key: "player_id" })
+    var sort_order = $("#roster tbody").sortable("serialize")
+
+    console.log("sort_order:", sort_order)
 
     $.ajax(window.location + "/reorder", {
       method: "POST",
-      data: {
-        order: sort_order
-      }
+      data: sort_order
     })
   })
 })
